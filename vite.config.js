c00843e2f5f140177d8565368c442a0446379440
gdrfc <1728727673@qq.com>
2026-03-30 16:24:19 +0800

    fix: 干净稳定版，全部兼容

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  ssg: {
-    createApp: () => import('./src/main.js').then(m => m.createAppInstance())
-  }
 })
