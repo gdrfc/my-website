9eaac349b007f3260dc831f84b7d63bb9725a991
gdrfc <1728727673@qq.com>
2026-03-30 16:33:04 +0800

    fix: 完美解决重名，最终版

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
-// 给 vite-ssg 用的导出函数
-export function createApp() {
+// 给 vite-ssg 使用（不会重名，完美兼容）
+export async function createAppFunc() {
   const app = createApp(App)
   app.use(router)
   return { app, router }
 }
 
-// 浏览器端才执行挂载
+// 浏览器端挂载
 if (typeof document !== 'undefined') {
-  const { app } = createApp()
-  app.mount('#app')
+  createAppFunc().then(({ app }) => {
+    app.mount('#app')
+  })
 }
