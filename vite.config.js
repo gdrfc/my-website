4096264f463c688c7752c8a7b654db9b4c536713
gdrfc <1728727673@qq.com>
2026-03-30 15:59:27 +0800

    fix: 正确适配vite-ssg格式

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { ViteSSG } from 'vite-ssg'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  ssg: {
+    createApp: () => import('./src/main.js').then(m => m.createAppInstance())
+  }
 })
