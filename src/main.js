0cc544a3446d60a06c063f8332190cac8f5441cf
gdrfc <1728727673@qq.com>
2026-03-30 14:44:14 +0800

    fix: 兼容vite-ssg，避免document未定义

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-// src/main.js  最终版！！！
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import './style.css'
 
-createApp(App).mount('#app')
+// 先创建 app 实例
+const app = createApp(App).use(router)
+
+// 只在浏览器环境执行 mount
+if (typeof document !== 'undefined') {
+  app.mount('#app')
+}
